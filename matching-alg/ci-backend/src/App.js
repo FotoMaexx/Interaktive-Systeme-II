4c99c4a28c492303c18f0539233c82edf006b1ed
Maximilian Hauser <info@hauser-media.com>
2024-10-16 17:21:07 +0200

    Updated Port

--- matching-alg/ci-backend/src/App.js
+++ matching-alg/ci-backend/src/App.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const ciRoutes = require('./routes/ciRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // CORS-Optionen festlegen
 const corsOptions = {
